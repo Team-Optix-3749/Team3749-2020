3c50607c5b9e37f1e0b006113c7c6bf479a11599
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-01-08 15:20:22 -0800

    remove un-needed buttons

--- Team3749-2020/src/main/java/frc/robot/RobotContainer.java
+++ Team3749-2020/src/main/java/frc/robot/RobotContainer.java
@@ -9,8 +9,6 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 import frc.robot.Constants;
 
@@ -41,39 +39,8 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
    * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private XboxController xboxCtrl;
-  private Joystick stick;
-  private JoystickButton jsButton1; //trigger on the joystick
-  private JoystickButton jsButton2;
-  private JoystickButton jsButton3;
-  private JoystickButton jsButton4;
-  private JoystickButton jsButton5;
-  private JoystickButton jsButton6;
-  private JoystickButton jsButton7;
-  private JoystickButton jsButton8;
-  private JoystickButton jsButton9;
-  private JoystickButton jsButton10;
-  private JoystickButton jsButton11;
-  private JoystickButton xbxButton1; //extra buttons
-  private JoystickButton xbxButton2;
-  private JoystickButton xbxButton3;
-  private JoystickButton xbxButton4;
-  private JoystickButton xbxButton5;
-  private JoystickButton xbxButton6;
 
   private void configureButtonBindings() {
-	stick = new Joystick(1);
-	jsButton1 = new JoystickButton(stick, 1);
-	jsButton2 = new JoystickButton(stick, 2);
-	jsButton3 = new JoystickButton(stick, 3);
-	jsButton4 = new JoystickButton(stick, 4);
-	jsButton5 = new JoystickButton(stick, 5);
-	jsButton6 = new JoystickButton(stick, 6);
-	jsButton7 = new JoystickButton(stick, 7);
-	jsButton8 = new JoystickButton(stick, 8);
-	jsButton9 = new JoystickButton(stick, 9);
-	jsButton10 = new JoystickButton(stick, 10);
-	jsButton11 = new JoystickButton(stick, 11);
 
 	if (Constants.xboxController.getAButtonPressed() == true){
 		Robot.getShooter().shoot(0.5);
