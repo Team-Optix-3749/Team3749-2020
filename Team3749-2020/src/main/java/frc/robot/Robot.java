dcf47369c30ccf10b8a87cbcfd691ebdae55ac0f
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-01-08 15:15:33 -0800

    remove un-needed robot controller

--- Team3749-2020/src/main/java/frc/robot/Robot.java
+++ Team3749-2020/src/main/java/frc/robot/Robot.java
@@ -22,8 +22,6 @@ import frc.robot.subsystems.*;
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
-  private RobotContainer m_robotContainer;
-
   private static Constants constants;
 
   private static Shooter shoot;
