a83cae817410bacd702f21725927e37c1a7dff15
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-01-08 15:14:53 -0800

    remove un-needed imports

--- Team3749-2020/src/main/java/frc/robot/RobotContainer.java
+++ Team3749-2020/src/main/java/frc/robot/RobotContainer.java
@@ -12,7 +12,6 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.subsystems.Shooter;
 
 import frc.robot.Constants;
 
