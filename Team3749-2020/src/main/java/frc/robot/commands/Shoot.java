f9f121d0fd1bb94873cdf512efbd40618c906f0c
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-01-08 19:37:45 -0800

    add execute to shoot command

--- Team3749-2020/src/main/java/frc/robot/commands/Shoot.java
+++ Team3749-2020/src/main/java/frc/robot/commands/Shoot.java
@@ -2,17 +2,20 @@ package frc.robot.commands;
 
 import frc.robot.subsystems.Shooter;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 
 public class Shoot extends CommandBase {
-  private final Shooter shooter;
+    private final Shooter shooter;
+    private VictorSPX shooterMotor;
 
-  public Shoot(Shooter shoot) {
-    shooter = shoot;
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(shooter);
-  }
+    public Shoot(Shooter shoot) {
+        shooter = shoot;
+        // Use addRequirements() here to declare subsystem dependencies.
+        addRequirements(shooter);
+    }
 
-  public void execute() {
-    
-  }
+    public void execute() {
+        shooterMotor.set(ControlMode.PercentOutput, 0.7);
+    }
 }
