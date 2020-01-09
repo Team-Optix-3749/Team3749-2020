4d4f3e9007774719b0f9fe282f1988b41f8e431b
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-01-08 19:41:05 -0800

    remove un-needed imports

--- Team3749-2020/src/main/java/frc/robot/commands/Shoot.java
+++ Team3749-2020/src/main/java/frc/robot/commands/Shoot.java
@@ -3,8 +3,6 @@ package frc.robot.commands;
 import frc.robot.Robot;
 import frc.robot.subsystems.Shooter;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import com.ctre.phoenix.motorcontrol.ControlMode;
 
 public class Shoot extends CommandBase {
     private final Shooter shooter;
