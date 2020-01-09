c520ca329e5b3ec389db3a69787ff149ee157c6e
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-01-08 19:56:08 -0800

    remove un-needed imports

--- Team3749-2020/src/main/java/frc/robot/RobotContainer.java
+++ Team3749-2020/src/main/java/frc/robot/RobotContainer.java
@@ -7,12 +7,6 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.button.Button;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.Constants;
-
 /**
  * This class is where the bulk of the robot should be declared. Since
  * Command-based is a "declarative" paradigm, very little robot logic should
@@ -49,7 +43,7 @@ public class RobotContainer {
     // if (Constants.xboxController.getAButtonPressed() == true) {
     //   Robot.getShooter().shoot(0.5);
     // }
-    
+
   }
 
   /**
