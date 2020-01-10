6c60be0c91c6d0ec9db341c637b5027e815000fc
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-01-09 21:03:07 -0800

    add content

---
+++ Team3749-2020/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Robot;
+
+public class Drivetrain extends SubsystemBase {
+
+  public Drivetrain() {
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
