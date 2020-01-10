42a730f2b7d330537b48031306a1d28eb66ec1e9
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-01-09 21:28:13 -0800

    add arcade drive

--- Team3749-2020/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ Team3749-2020/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Robot;
@@ -16,6 +17,12 @@ public class Drivetrain extends SubsystemBase {
     private WPI_TalonSRX motorRightFront = new WPI_TalonSRX(Constants.motorRightFront);
     private WPI_TalonSRX motorRightBack = new WPI_TalonSRX(Constants.motorRightBack);
     public SpeedControllerGroup rightSideMotors = new SpeedControllerGroup(motorRightFront, motorRightBack);
+
+    private DifferentialDrive drive = new DifferentialDrive(leftSideMotors, rightSideMotors);
+
+    public void arcadeDrive() {
+        drive.arcadeDrive(Constants.LSX, Constants.LSY);
+    }
 
     @Override
     public void periodic() {
