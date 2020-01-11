209a798737f7f7994c1e98e7daabd25eae51e99e
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-01-10 17:38:44 -0800

    create CANEncoder

--- Team3749-2020/src/main/java/frc/robot/subsystems/shooter.java
+++ Team3749-2020/src/main/java/frc/robot/subsystems/shooter.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Shooter extends SubsystemBase {
   private VictorSPX shooterMotor;
 
+  CANEncoder encoder;
   CANPIDController controller;
 
   double setpoint = 0;
