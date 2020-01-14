9d3c1c8b51c875b255209621ba642452cbc6fbd2
ishitav <ishitavas@gmail.com>
2020-01-14 14:13:16 -0800

    adding the lifting mechanism subsystem

---
+++ Team3749-2020/src/main/java/frc/robot/subsystems/Lifting.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class Lifting extends SubsystemBase {
+    //include more specific code and stuff, setting up the stuff for the lifting mechanism team-skywalker
+}
