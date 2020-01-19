44324e5c70e93242c0e3ea96c576f522916be507
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-01-18 22:43:55 -0800

    format code

--- Team3749-2020/src/main/java/frc/robot/Robot.java
+++ Team3749-2020/src/main/java/frc/robot/Robot.java
@@ -43,20 +43,9 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer. This will perform all our button bindings,
-    // and put our
-    // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
 
     initCamera();
-  }
-
-  public void initCamera() {
-    // start running camera from roboRIO
-    UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
-    camera.setBrightness(8);
-    camera.setExposureManual(5);
-    // camera.setExposureAuto();
   }
 
   public static RobotContainer getRobotContainer() {
@@ -75,9 +64,13 @@ public class Robot extends TimedRobot {
     return m_shoot;
   }
 
-  // public static BallCollect getBallCollect() {
-  // return collect;
-  // }
+  public void initCamera() {
+    // start running camera from roboRIO
+    UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+    camera.setBrightness(8);
+    camera.setExposureManual(5);
+    // camera.setExposureAuto();
+  }
 
   /**
    * This function is called every robot packet, no matter the mode. Use this for
