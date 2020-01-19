525016dbee1f5ffe16a9babb7240cdf358fabc3c
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-01-18 19:55:18 -0800

    add xbox controller port

--- Team3749-2020/src/main/java/frc/robot/Constants.java
+++ Team3749-2020/src/main/java/frc/robot/Constants.java
@@ -48,6 +48,8 @@ public final class Constants {
     */
 
     // Xbox Map
+    public static final int xboxControllerPort = 0;
+
     public static final int A = 1;
     public static final int B = 2;
     public static final int X = 3;
