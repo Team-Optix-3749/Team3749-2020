cd68e6d5032ca337636a8f7aadaf2ca27110bf07
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-01-18 19:45:23 -0800

    add shoot command

--- Team3749-2020/src/main/java/frc/robot/commands/Shoot.java
+++ Team3749-2020/src/main/java/frc/robot/commands/Shoot.java
@@ -1,6 +1,5 @@
 package frc.robot.commands;
 
-import frc.robot.Constants;
 import frc.robot.Robot;
 import frc.robot.subsystems.Shooter;
 import edu.wpi.first.wpilibj2.command.CommandBase;
@@ -8,18 +7,13 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 public class Shoot extends CommandBase {
     private final Shooter shooter;
 
-    public Shoot(final Shooter shoot) {
+    public Shoot(Shooter shoot) {
         shooter = shoot;
         addRequirements(shooter);
     }
 
     @Override
     public void execute() {
-        // Robot.getShooter().set(Constants.shooterSetpoint);
-    }
-
-    @Override
-    public boolean isFinished() {
-        return false;
+        Robot.getShooter().Shoot();
     }
 }
