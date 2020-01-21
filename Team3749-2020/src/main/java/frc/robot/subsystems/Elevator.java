c824fb1913347a4af5d8c6fd31a772806c6c07be
ishitav <ishitavas@gmail.com>
2020-01-21 13:45:43 -0800

    testing somethings out

--- Team3749-2020/src/main/java/frc/robot/subsystems/Elevator.java
+++ Team3749-2020/src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,11 +1,15 @@
 package frc.robot.subsystems;
 
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
 public class Elevator extends SubsystemBase {
     //include more specific code and stuff, setting up the stuff for the lifting mechanism team-skywalker
-   
+    private WPI_TalonSRX leftMotorControl;
+    private WPI_TalonSRX rightMotorControl;
+
     @Override
     public void periodic() {
       // This method will be called once per scheduler run
@@ -14,7 +18,7 @@ public class Elevator extends SubsystemBase {
     public void moveToTop() {
 
     }
-
+ 
     public void moveToBottom() {
 
     }
