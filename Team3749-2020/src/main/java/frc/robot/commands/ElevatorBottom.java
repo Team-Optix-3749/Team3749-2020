01809cbfd8d4ad46b5b228ac61a2393a66d92cdd
ishitav <ishitavas@gmail.com>
2020-01-30 12:45:46 -0800

    Added Two new commands for elevator

---
+++ Team3749-2020/src/main/java/frc/robot/commands/ElevatorBottom.java
@@ -0,0 +1,43 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Elevator;
+
+/**
+ * An example command that uses an example subsystem.
+ */
+public class ElevatorBottom extends CommandBase {
+    @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
+    public final Elevator[] m_subsystem;
+
+    /**
+     * Creates a new ExampleCommand.
+     *
+     * @param subsystem The subsystem used by this command.
+     */
+
+    public ElevatorBottom(Elevator[] subsystem) {
+    m_subsystem = subsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    //addRequirements(subsystem);
+  }
+  // want to see which branch this pushes to 
+
+  @Override
+  public void initialize() {
+      //initialization code here
+  }
+
+  @Override
+  public void execute() {
+      //add whatever code u wanna execute here
+  }
+
+  @Override
+  public boolean isFinished() {
+      //when should this command be terminated
+      return false; 
+  }
+  
+ 
+}
