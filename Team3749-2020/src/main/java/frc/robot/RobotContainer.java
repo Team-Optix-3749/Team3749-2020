f6db80e5194d0a6d4e1381982d98a988319369f9
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-01-31 11:55:29 -0800

    set default command as Drive.java

--- Team3749-2020/src/main/java/frc/robot/RobotContainer.java
+++ Team3749-2020/src/main/java/frc/robot/RobotContainer.java
@@ -43,27 +43,27 @@ public class RobotContainer {
   public RobotContainer() {
     configureButtonBindings();
 
-        // Get the x speed. We are inverting this because Xbox controllers return
+    // Get the x speed. We are inverting this because Xbox controllers return
     // negative values when we push forward.
     final var xSpeed =
-        -m_speedLimiter.calculate(Robot.getRobotContainer().m_xboxController.getY(Hand.kLeft))
-            * Robot.getConstants().kMaxSpeed;
+      -m_speedLimiter.calculate(Robot.getRobotContainer().m_xboxController.getY(Hand.kLeft))
+        * Robot.getConstants().kMaxSpeed;
 
     // Get the rate of angular rotation. We are inverting this because we want a
     // positive value when we pull to the left (remember, CCW is positive in
     // mathematics). Xbox controllers return positive values when you pull to
     // the right by default.
     final var rot =
-        -m_rotLimiter.calculate(Robot.getRobotContainer().m_xboxController.getX(Hand.kRight))
-            * Robot.getConstants().kMaxAngularSpeed;
+      -m_rotLimiter.calculate(Robot.getRobotContainer().m_xboxController.getX(Hand.kRight))
+        * Robot.getConstants().kMaxAngularSpeed;
 
     m_drive.setDefaultCommand(new Drive(m_drive, xSpeed, rot));
 
     // m_drive.setDefaultCommand(
-    //     new ArcadeDrive(
-    //         m_drive,
-    //         () -> m_xboxController.getY(Hand.kLeft),
-    //         () -> m_xboxController.getX(Hand.kRight)));
+    //    new ArcadeDrive(
+    //      m_drive,
+    //      () -> m_xboxController.getY(Hand.kLeft),
+    //      () -> m_xboxController.getX(Hand.kRight)));
   }
 
   private void configureButtonBindings() {
