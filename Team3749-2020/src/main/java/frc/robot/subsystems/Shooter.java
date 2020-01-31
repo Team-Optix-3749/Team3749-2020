7c457f19ce1b351d978d7949a9f17e0d68413308
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-01-30 21:42:12 -0800

    change imports

--- Team3749-2020/src/main/java/frc/robot/subsystems/Shooter.java
+++ Team3749-2020/src/main/java/frc/robot/subsystems/Shooter.java
@@ -2,23 +2,27 @@ package frc.robot.subsystems;
 
 import frc.robot.Robot;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.DemandType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+/**
+ * class Shooter
+ * @author Raadwan Masum
+ */
 public class Shooter extends SubsystemBase {
-  private WPI_TalonSRX m_shooterMotor;
+  private TalonSRX m_shooterMotor;
 
   public Shooter() {
-    m_shooterMotor = new WPI_TalonSRX(Robot.getConstants().getCAN("shooter_motor"));
+    m_shooterMotor = new TalonSRX(Robot.getConstants().getCAN("shooter_motor"));
   }
 
   // fix this:
   public void pidSpeed(double feedforward){
     double targetPos = 1;
-    m_shooterMotor.set(ControlMode.MotionMagic, targetPos, DemandType.ArbitraryFeedForward, feedforward);
+    m_shooterMotor.set(ControlMode.Velocity, );
   }
   
   private void setSpeed(double speed){
