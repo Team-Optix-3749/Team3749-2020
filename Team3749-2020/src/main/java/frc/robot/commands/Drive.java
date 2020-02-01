39da62c15269d01041e34f80606f774179422bf3
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-02-01 13:24:30 -0800

    comment out everything

--- Team3749-2020/src/main/java/frc/robot/commands/Drive.java
+++ Team3749-2020/src/main/java/frc/robot/commands/Drive.java
@@ -1,21 +1,21 @@
-package frc.robot.commands;
+// package frc.robot.commands;
 
-import frc.robot.subsystems.Drivetrain;
+// import frc.robot.subsystems.Drivetrain;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+// import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class Drive extends CommandBase {
-    private final Drivetrain m_drive;
-    private double xSpeed;
-    private double rot;
+// public class Drive extends CommandBase {
+//     private final Drivetrain m_drive;
+//     private double xSpeed;
+//     private double rot;
 
-    public Drive(Drivetrain drive, double xSpeed, double rot) {
-        m_drive = drive;
-        addRequirements(m_drive);
-    }
+//     public Drive(Drivetrain drive, double xSpeed, double rot) {
+//         m_drive = drive;
+//         addRequirements(m_drive);
+//     }
 
-    @Override
-    public void execute() {
-        m_drive.drive(xSpeed, rot);
-    }
-}
+//     @Override
+//     public void execute() {
+//         m_drive.drive(xSpeed, rot);
+//     }
+// }
