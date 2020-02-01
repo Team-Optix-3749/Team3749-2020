2afd5e640638eba8882a83f18141d99cf547110e
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-02-01 13:26:38 -0800

    comment out vision, it's broken

--- Team3749-2020/src/main/java/frc/robot/subsystems/Vision.java
+++ Team3749-2020/src/main/java/frc/robot/subsystems/Vision.java
@@ -1,14 +1,14 @@
-package frc.robot.subsystems;
+// package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.I2C;
-import edu.wpi.first.wpilibj.I2C.Port;
-import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+// import edu.wpi.first.wpilibj.I2C;
+// import edu.wpi.first.wpilibj.I2C.Port;
+// import edu.wpi.first.wpilibj.command.Subsystem;
+// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- *
- */
-public class Vision extends Subsystem {
+// /**
+//  *
+//  */
+// public class Vision extends Subsystem {
 
-    public Pixy2 pixy = Pixy2.createInstance(2);
-}
+//     public Pixy2 pixy = Pixy2.createInstance(2);
+// }
