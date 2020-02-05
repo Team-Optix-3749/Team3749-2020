4d245ae4028efe8fee0547c41183eac5c99a3645
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-02-04 20:30:12 -0800

    format code

--- Team3749-2020/src/main/java/frc/robot/subsystems/Shooter.java
+++ Team3749-2020/src/main/java/frc/robot/subsystems/Shooter.java
@@ -9,6 +9,7 @@ import frc.robot.Robot;
 
 /**
  * class Shooter
+ * 
  * @author Raadwan Masum
  */
 public class Shooter extends SubsystemBase {
@@ -17,73 +18,74 @@ public class Shooter extends SubsystemBase {
 
   public Shooter() {
     m_shooterMotor = new TalonSRX(Robot.getConstants().getCAN("shooter_motor"));
-    
+
     /* Factory Default all hardware to prevent unexpected behaviour */
     m_shooterMotor.configFactoryDefault();
 
-		/* Config sensor used for Primary PID [Velocity] */
-    m_shooterMotor.configSelectedFeedbackSensor(
-      FeedbackDevice.CTRE_MagEncoder_Relative, 
-      Robot.getConstants().kPIDLoopIdx, 
-      Robot.getConstants().kTimeoutMs);
+    /* Config sensor used for Primary PID [Velocity] */
+    m_shooterMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative,
+        Robot.getConstants().kPIDLoopIdx, Robot.getConstants().kTimeoutMs);
 
     /**
-     * Phase sensor accordingly. 
-     * Positive Sensor Reading should match Green (blinking) Leds on Talon
+     * Phase sensor accordingly. Positive Sensor Reading should match Green
+     * (blinking) Leds on Talon
      */
-		m_shooterMotor.setSensorPhase(true);
+    m_shooterMotor.setSensorPhase(true);
 
-		/* Config the peak and nominal outputs */
-		m_shooterMotor.configNominalOutputForward(0, Robot.getConstants().kTimeoutMs);
-		m_shooterMotor.configNominalOutputReverse(0, Robot.getConstants().kTimeoutMs);
-		m_shooterMotor.configPeakOutputForward(1, Robot.getConstants().kTimeoutMs);
-		m_shooterMotor.configPeakOutputReverse(-1, Robot.getConstants().kTimeoutMs);
+    /* Config the peak and nominal outputs */
+    m_shooterMotor.configNominalOutputForward(0, Robot.getConstants().kTimeoutMs);
+    m_shooterMotor.configNominalOutputReverse(0, Robot.getConstants().kTimeoutMs);
+    m_shooterMotor.configPeakOutputForward(1, Robot.getConstants().kTimeoutMs);
+    m_shooterMotor.configPeakOutputReverse(-1, Robot.getConstants().kTimeoutMs);
 
-		/* Config the Velocity closed loop gains in slot0 */
-		m_shooterMotor.config_kF(Robot.getConstants().kPIDLoopIdx, Robot.getConstants().kGains_Velocit.kF, Robot.getConstants().kTimeoutMs);
-		m_shooterMotor.config_kP(Robot.getConstants().kPIDLoopIdx, Robot.getConstants().kGains_Velocit.kP, Robot.getConstants().kTimeoutMs);
-		m_shooterMotor.config_kI(Robot.getConstants().kPIDLoopIdx, Robot.getConstants().kGains_Velocit.kI, Robot.getConstants().kTimeoutMs);
-		m_shooterMotor.config_kD(Robot.getConstants().kPIDLoopIdx, Robot.getConstants().kGains_Velocit.kD, Robot.getConstants().kTimeoutMs);
+    /* Config the Velocity closed loop gains in slot0 */
+    m_shooterMotor.config_kF(Robot.getConstants().kPIDLoopIdx, Robot.getConstants().kGains_Velocit.kF,
+        Robot.getConstants().kTimeoutMs);
+    m_shooterMotor.config_kP(Robot.getConstants().kPIDLoopIdx, Robot.getConstants().kGains_Velocit.kP,
+        Robot.getConstants().kTimeoutMs);
+    m_shooterMotor.config_kI(Robot.getConstants().kPIDLoopIdx, Robot.getConstants().kGains_Velocit.kI,
+        Robot.getConstants().kTimeoutMs);
+    m_shooterMotor.config_kD(Robot.getConstants().kPIDLoopIdx, Robot.getConstants().kGains_Velocit.kD,
+        Robot.getConstants().kTimeoutMs);
 
     reset();
   }
-  
-  private void rawSpeed(double speed){
+
+  private void rawSpeed(double speed) {
     m_shooterMotor.set(ControlMode.PercentOutput, speed);
   }
 
-  public void shoot(){
+  public void shoot() {
     rawSpeed(Robot.getConstants().kShooterSpeed);
   }
 
   /**
    * Set desired shooter velocity
-   * @param velocity desired velocity
+   * 
+   * @param velocity desired veloci
    */
   public void setVelocity(double velocity) {
-    	/* Velocity Closed Loop */
+    /* Velocity Closed Loop */
 
-			/**
-			 * Convert 500 RPM to units / 100ms.
-			 * 4096 Units/Rev * 500 RPM / 600 100ms/min in either direction:
-			 * velocity setpoint is in units/100ms
-			 */
+    /**
+     * Convert 500 RPM to units / 100ms. 4096 Units/Rev * 500 RPM / 600 100ms/min in
+     * either direction: velocity setpoint is in units/100ms
+     */
 
-      // double targetVelocity_UnitsPer100ms = leftYstick * 500.0 * 4096 / 600;
-      
-			/* 500 RPM in either direction */
-			m_shooterMotor.set(ControlMode.Velocity, velocity);
+    // double targetVelocity_UnitsPer100ms = leftYstick * 500.0 * 4096 / 600;
+
+    /* 500 RPM in either direction */
+    m_shooterMotor.set(ControlMode.Velocity, velocity);
   }
 
-  public void stop(){
+  public void stop() {
     rawSpeed(0);
   }
 
-  public void reset()
-  {
+  public void reset() {
     rawSpeed(0);
   }
-  
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
