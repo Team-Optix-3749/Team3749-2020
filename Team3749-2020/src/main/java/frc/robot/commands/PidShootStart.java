e8c744c64673799da16852f6e0975574dc63cdb4
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-02-04 20:06:22 -0800

    fix pid in command

--- Team3749-2020/src/main/java/frc/robot/commands/PidShootStart.java
+++ Team3749-2020/src/main/java/frc/robot/commands/PidShootStart.java
@@ -35,7 +35,7 @@ public class PidShootStart extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_shooter.setVelocity(vel);
+    m_shooter.pidShoot(vel);
   }
 
   // Called once the command ends or is interrupted.
