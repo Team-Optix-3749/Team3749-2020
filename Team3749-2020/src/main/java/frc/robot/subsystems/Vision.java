fe635125908158b8a50941038ee1d53d3fdc611c
David Ryan <dryan0319@gmail.com>
2020-02-04 20:54:02 -0800

    Update Vision.java

--- Team3749-2020/src/main/java/frc/robot/subsystems/Vision.java
+++ Team3749-2020/src/main/java/frc/robot/subsystems/Vision.java
@@ -1,14 +1,20 @@
-// package frc.robot.subsystems;
+package frc.robot.subsystems;
 
-// import edu.wpi.first.wpilibj.I2C;
-// import edu.wpi.first.wpilibj.I2C.Port;
-// import edu.wpi.first.wpilibj.command.Subsystem;
-// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import io.github.pseudoresonance.pixy2api.Pixy2;
+import io.github.pseudoresonance.pixy2api.links.SPILink;
 
-// /**
-//  *
-//  */
-// public class Vision extends Subsystem {
+/**
+ *
+ */
+public class Vision extends Subsystem {
 
-//     public Pixy2 pixy = Pixy2.createInstance(2);
-// }
+    public Pixy2 pixy = Pixy2.createInstance(new SPILink());
+
+    @Override
+    protected void initDefaultCommand() {
+        // TODO Auto-generated method stub
+
+    }
+}
