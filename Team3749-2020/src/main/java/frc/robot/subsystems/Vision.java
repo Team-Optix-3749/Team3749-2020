d77945d632d908c842b764ae5b96048264300a77
tonyhieu <54915685+tonyhieu@users.noreply.github.com>
2020-02-04 16:35:53 -0800

    created vision subsystem w/ ntables

---
+++ Team3749-2020/src/main/java/frc/robot/subsystems/Vision.java
@@ -0,0 +1,40 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+public class Vision extends SubsystemBase {
+
+  private NetworkTableInstance inst = new NetworkTableInstance;
+  private NetworkTable table = new NetworkTable;
+  private NetworkTableEntry xEntry = new NetworkTableEntry;
+  private NetworkTableEntry yEntry = new NetworkTableEntry;
+  private NetworkTableEntry distanceEntry = new NetworkTableEntry;
+  
+  public Vision() {
+    inst = NetworkTableInstance.getDefault();
+    table = inst.getTable("example");
+    xEntry = networkTable.getEntry("X");
+    yEntry = networkTable.getEntry("Y");
+    distanceEntry = networkTable.getEntry("Distance");
+  }
+
+  public double getX() {
+    return xEntry.getDouble();
+  }
+
+  public double getY() {
+    return yEntry.getDouble();
+  }
+
+  public double getDist() {
+    return distanceEntry.getDouble();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
