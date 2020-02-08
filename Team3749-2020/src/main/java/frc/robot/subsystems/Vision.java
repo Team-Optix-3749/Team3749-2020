e7a50df04ecfe018a7faf0ef3da7c5162c390b7e
David Ryan <dryan0319@gmail.com>
2020-02-08 13:07:58 -0800

    added code to receive and parse data from arduino

--- Team3749-2020/src/main/java/frc/robot/subsystems/Vision.java
+++ Team3749-2020/src/main/java/frc/robot/subsystems/Vision.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.SerialPort;
+//import edu.wpi.first.wpilibj.SerialPort.Port;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -8,16 +9,39 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Vision extends Subsystem {
 
-    private SerialPort serialport;
+    private SerialPort m_arduinoPort;
+    private String message;
+    private int m_x, m_y;
+    private double m_distance;
 
     public Vision() {
-        serialport = new SerialPort(115200, SerialPort.Port.kUSB);
+        m_arduinoPort = new SerialPort(115200, SerialPort.Port.kUSB);
+        message = new String("");
+        m_x = 0;
+        m_y = 0;
     }
 
-	@Override
-	protected void initDefaultCommand() {
-		// TODO Auto-generated method stub
-		
-	}
+    public void readData() {
+        message = m_arduinoPort.readString();
+        m_x = Integer.parseInt(message.substring(0, 1));
+        m_y = Integer.parseInt(message.substring(message.indexOf("a")+1,message.indexOf("b")));
+        m_distance = Double.parseDouble(message.substring(message.indexOf("b")+1));
+        System.out.print("x = " + m_x + " y = " + m_y + " distance = " + m_distance);
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    protected void initDefaultCommand() {
+        // TODO Auto-generated method stub
+
+    }
+
 }
 
