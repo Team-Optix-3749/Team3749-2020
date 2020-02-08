d31194d1892b477baeda0aa413ca89c423be5958
David Ryan <dryan0319@gmail.com>
2020-02-08 13:30:24 -0800

    Added NetworkTables code

--- Team3749-2020/src/main/java/frc/robot/subsystems/Vision.java
+++ Team3749-2020/src/main/java/frc/robot/subsystems/Vision.java
@@ -1,7 +1,10 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.SerialPort;
-//import edu.wpi.first.wpilibj.SerialPort.Port;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -14,11 +17,23 @@ public class Vision extends Subsystem {
     private int m_x, m_y;
     private double m_distance;
 
+    private NetworkTableInstance inst;
+    private NetworkTable table;
+    private NetworkTableEntry xEntry;
+    private NetworkTableEntry yEntry;
+    private NetworkTableEntry distanceEntry;
+
     public Vision() {
         m_arduinoPort = new SerialPort(115200, SerialPort.Port.kUSB);
         message = new String("");
         m_x = 0;
         m_y = 0;
+
+        inst = NetworkTableInstance.getDefault();
+        table = inst.getTable("example");
+        xEntry = table.getEntry("X");
+        yEntry = table.getEntry("Y");
+        distanceEntry = table.getEntry("Distance");
     }
 
     public void readData() {
@@ -26,8 +41,35 @@ public class Vision extends Subsystem {
         m_x = Integer.parseInt(message.substring(0, 1));
         m_y = Integer.parseInt(message.substring(message.indexOf("a")+1,message.indexOf("b")));
         m_distance = Double.parseDouble(message.substring(message.indexOf("b")+1));
-        System.out.print("x = " + m_x + " y = " + m_y + " distance = " + m_distance);
+        
+        setX(m_x);
+        setY(m_y);
+        setDist(m_distance);
     }
+
+    public double getX() {
+        return xEntry.getDouble(0);
+      }
+    
+      public double getY() {
+        return yEntry.getDouble(0);
+      }
+    
+      public double getDist() {
+        return distanceEntry.getDouble(0);
+      }
+    
+      public void setX(int entry) {
+        xEntry.forceSetDouble(entry);
+      }
+    
+      public void setY(int entry) {
+        yEntry.forceSetDouble(entry);
+      }
+    
+      public void setDist(double entry) {
+        distanceEntry.forceSetDouble(entry);
+      }
 
     public String getMessage() {
         return message;
