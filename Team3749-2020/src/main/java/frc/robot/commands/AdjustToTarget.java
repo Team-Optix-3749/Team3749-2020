d8468e14b0855916dafeadb64911bed3b5ae006a
chenxin-chex <chenxinni0@gmail.com>
2020-02-15 11:09:26 -0800

    fixed adjusttotarget

--- Team3749-2020/src/main/java/frc/robot/commands/AdjustToTarget.java
+++ Team3749-2020/src/main/java/frc/robot/commands/AdjustToTarget.java
@@ -1,7 +1,7 @@
 package frc.robot.commands;
 
 import java.util.function.DoubleSupplier;
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Vision;
 import frc.robot.commands.ArcadeDrive;
@@ -9,7 +9,7 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
-public class AdjustToTarget extends Command {
+public class AdjustToTarget extends CommandBase {
     private final Drivetrain m_drive;
     private final Vision m_vision;
     //private final DoubleSupplier m_forward;
@@ -29,6 +29,14 @@ public class AdjustToTarget extends Command {
            return networkTable.getEntry("x").getDouble(-1);
     }
 
+    public double getdistance() {
+        NetworkTableInstance networkTableInstance = NetworkTableInstance.getDefault();
+        NetworkTable networkTable;
+        networkTableInstance.startServer();
+        networkTable = networkTableInstance.getTable("networkTable");
+            return networkTable.getEntry("distance").getDouble(10);
+     }
+
     //continueRotating = true;
     //if(continueRotating==true){
     //   rotate();
@@ -36,20 +44,16 @@ public class AdjustToTarget extends Command {
      //   }
 
 
-   
-    private void rotate() {
+    @Override
+    public void execute() {
        Object table;
        double error = getx()-1;
        if (getx() != -1) {
            m_drive.arcadeDrive(0, 0.3 * error);
         }
+        if (getdistance() != 10) {
+            m_drive.arcadeDrive(0.3, 0);
+           }
     }
 
-   
-
-   @Override
-    protected boolean isFinished() {
-        // TODO Auto-generated method stub
-        return false;
-   }
 }
