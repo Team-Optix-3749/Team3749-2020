6db270b3f16ca30cc76baf889f8fbff8e94e9ea2
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-02-17 07:43:04 -0800

    fix typos

--- Team3749-2020/src/main/java/frc/robot/commands/IndexStop.java
+++ Team3749-2020/src/main/java/frc/robot/commands/IndexStop.java
@@ -9,7 +9,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-import frc.robot.subsystems.Intake;;;
+import frc.robot.subsystems.Intake;
 
 public class IndexStop extends CommandBase {
     private final Intake m_intake;
