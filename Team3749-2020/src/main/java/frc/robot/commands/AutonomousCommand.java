5580374d0e8706feebf6255587230a89436e7a8f
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-02-16 19:02:54 -0800

    remove unneeded imports

--- Team3749-2020/src/main/java/frc/robot/commands/AutonomousCommand.java
+++ Team3749-2020/src/main/java/frc/robot/commands/AutonomousCommand.java
@@ -1,8 +1,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-
 
 /**
  * An example command that uses an example subsystem.
