2d31f9c30af38488dbd386fae12985b1e20cc7e7
David Ryan <dryan0319@gmail.com>
2020-02-22 21:34:41 -0800

    made changes

--- Team3749-2020/src/main/java/frc/robot/commands/TargetDataStart.java
+++ Team3749-2020/src/main/java/frc/robot/commands/TargetDataStart.java
@@ -10,14 +10,14 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Vision;
 
-public class TargetDataStop extends CommandBase {
+public class TargetData extends CommandBase {
   private final Vision m_vision;
 
   /**
    *   
    * @param subsystem
    */
-  public TargetDataStop(Vision subsystem) {
+  public TargetDataStart(Vision subsystem) {
     m_vision = subsystem;
   }
 
@@ -30,13 +30,13 @@ public class TargetDataStop extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_vision.stop();
+    m_vision.start();
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-
+      
   }
 
   // Returns true when the command should end.
--- Team3749-2020/src/main/java/frc/robot/commands/TargetGetData.java
+++ Team3749-2020/src/main/java/frc/robot/commands/TargetGetData.java
@@ -8,40 +8,33 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.Vision;
 
-public class TargetDataStop extends CommandBase {
-  private final Vision m_vision;
-
+public class TargetGetData extends CommandBase {
   /**
-   *   
-   * @param subsystem
+   * Creates a new TargetGetData.
    */
-  public TargetDataStop(Vision subsystem) {
-    m_vision = subsystem;
+  public TargetGetData() {
+    // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    
   }
-  
+
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_vision.stop();
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return true;
+    return false;
   }
 }
