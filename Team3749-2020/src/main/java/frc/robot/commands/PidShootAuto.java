e88b4df092627a10cf09b387a31f15df6b42b15c
David Ryan <dryan0319@gmail.com>
2020-02-23 20:10:21 -0800

    made change to formula

--- Team3749-2020/src/main/java/frc/robot/commands/PidShootAuto.java
+++ Team3749-2020/src/main/java/frc/robot/commands/PidShootAuto.java
@@ -42,7 +42,7 @@ public class PidShootAuto extends CommandBase {
 
  //Calculates and returns the angular velocity needed based on distance away from target
  public double getVelocity() {
-   double xDisplacement = getdistance() + .207;
+   double xDisplacement = (getdistance() * 0.0254) + .207;
    double yDisplacement = 2.49 - Robot.getConstants().kShooterHeight;
 
    double velocity = (-4.9 * Math.pow(xDisplacement, 2) * Math.pow(Math.acos(Robot.getConstants().kShooterAngle), 2)) /
