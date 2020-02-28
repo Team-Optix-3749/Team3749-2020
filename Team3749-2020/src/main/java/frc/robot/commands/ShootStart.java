4c5dfcdca4d8bf2879435e79b6e6a3ab8b7e589d
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-02-27 17:40:47 -0800

    add shooter stop

--- Team3749-2020/src/main/java/frc/robot/commands/ShootStart.java
+++ Team3749-2020/src/main/java/frc/robot/commands/ShootStart.java
@@ -37,6 +37,9 @@ public class ShootStart extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    if (interrupted){
+      m_shooter.stop();
+    }
   }
 
   // Returns true when the command should end.
