7a14864ad225febb3c1fe1b867dd902792272c62
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-02-29 12:12:24 -0800

    remove random import

--- Team3749-2020/src/main/java/frc/robot/RobotContainer.java
+++ Team3749-2020/src/main/java/frc/robot/RobotContainer.java
@@ -9,8 +9,6 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 
-import javax.net.ssl.TrustManagerFactorySpi;
-
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController.Button;
