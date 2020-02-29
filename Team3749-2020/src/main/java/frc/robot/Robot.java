f6b2511be15d79eefdf5928e64fc756de7783f4f
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-02-29 13:33:37 -0800

    remove example comments

--- Team3749-2020/src/main/java/frc/robot/Robot.java
+++ Team3749-2020/src/main/java/frc/robot/Robot.java
@@ -172,10 +172,8 @@ public class Robot extends TimedRobot {
   public void autonomousInit() {
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
-    // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
-
     }
 
   }
