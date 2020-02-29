a3edb5a77adaed2350453c355e521bd9bdd342c2
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-02-29 13:24:41 -0800

    add drivetrain

--- Team3749-2020/src/main/java/frc/robot/commands/AutonomousCommand.java
+++ Team3749-2020/src/main/java/frc/robot/commands/AutonomousCommand.java
@@ -1,22 +1,24 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.*;
 
 /**
  * An example command that uses an example subsystem.
  */
 public class AutonomousCommand extends CommandBase {
     private final Shooter m_shooter;
+    private final Drivetrain m_drivetrain;
 
     /**
      * Creates a new ExampleCommand.
      *
      * @param subsystem The subsystem used by this command.
      */
-    public AutonomousCommand(Shooter subsystem) {
-        m_shooter = subsystem;
-        addRequirements(m_shooter);
+    public AutonomousCommand(Drivetrain drivetrain, Shooter shooter) {
+        m_drivetrain = drivetrain;
+        m_shooter = shooter;
+        addRequirements(m_drivetrain, m_shooter);
     }
 
     @Override
