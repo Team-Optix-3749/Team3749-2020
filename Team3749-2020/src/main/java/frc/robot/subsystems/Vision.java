8ee58033a60af1ce1eae127e64a31b8d9701a0ac
davidry777 <42628566+davidry777@users.noreply.github.com>
2020-03-01 14:33:09 -0800

    adjusted pixy code

--- Team3749-2020/src/main/java/frc/robot/subsystems/Vision.java
+++ Team3749-2020/src/main/java/frc/robot/subsystems/Vision.java
@@ -1,12 +1,50 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.networktables.*;
+import edu.wpi.first.wpilibj.SerialPort;
 
 public class Vision {
   private NetworkTable table;
+  private SerialPort m_arduinoPort;
+  private double pixy_xVal = 0;
 
   public Vision() {
     table = NetworkTableInstance.getDefault().getTable("limelight");
+
+    /*Instantiating arduino port as SerialPort, trying every instantiation
+        with kUSB, kUSB1, and kUSB2. We used try-catch to see which instantiation failed*/
+        try {
+          m_arduinoPort = new SerialPort(115200, SerialPort.Port.kUSB);
+          System.out.println("Connected to arduino in vision!");
+        } catch (Exception e) {
+          System.out.println("kUSB failed");
+    
+          try {
+            m_arduinoPort = new SerialPort(115200, SerialPort.Port.kUSB1);
+            System.out.println("Connected to arduino in vision!");
+          } catch (Exception e1) {
+            System.out.println("kUSB1 failed");
+            
+            try {
+              m_arduinoPort = new SerialPort(115200, SerialPort.Port.kUSB2);
+              System.out.println("Connected to arduino in vision!"); 
+            } catch(Exception e2){
+              System.out.println("kUSB2 failed");
+            }
+          }
+        }
+  }
+
+  public void readData() {
+    try {
+      pixy_xVal = Integer.parseInt(m_arduinoPort.readString());
+    } catch(Exception e) {
+      pixy_xVal = 0;
+    }
+  }
+
+  public double getPixyXValue() {
+    return pixy_xVal;
   }
 
   public double getAngleX() {
