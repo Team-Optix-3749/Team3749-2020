aa2e22276e357ffff3f0c2e40369d1540e907de5
davidry777 <42628566+davidry777@users.noreply.github.com>
2020-03-02 21:12:55 -0800

    created ball alignment command

--- Team3749-2020/src/main/java/frc/robot/commands/IntakeAlign.java
+++ Team3749-2020/src/main/java/frc/robot/commands/IntakeAlign.java
@@ -8,37 +8,38 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Vision;
 
-import frc.robot.subsystems.Intake;
-
-public class IntakeAuto extends CommandBase {
-    private final Intake m_intake;
-    private double speed;
-    private long start;
+public class IntakeAlign extends CommandBase {
+    private final Drivetrain m_drive;
+    private final Vision m_vision;
 
     /**
      * 
-     * @param subsystem subsystem
+     * @param subsystem subsystem1
+     * @param subsystem subsystem2
      * @param speed set speed
      */
-    public IntakeAuto(Intake subsystem, double set) {
-        m_intake = subsystem;
-        addRequirements(m_intake);
-
-        speed = set;
+    public IntakeAlign(Drivetrain subsystem1, Vision subsystem2) {
+        m_drive = subsystem1;
+        m_vision = subsystem2;
+        addRequirements(m_drive);
     }
 
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
-        start = System.currentTimeMillis();
+        //start = System.currentTimeMillis();
     }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        if(System.currentTimeMillis() - start > 5000)
-            m_intake.setIntake(speed);
+        if(m_vision.getPixyXValue() > 158)
+            m_drive.arcadeDrive(0, -0.3);
+        if(m_vision.getPixyXValue() < 158)
+            m_drive.arcadeDrive(0, 0.3);
     }
 
     // Called once the command ends or is interrupted.
@@ -49,9 +50,8 @@ public class IntakeAuto extends CommandBase {
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        if(System.currentTimeMillis() - start > 10000)
+        if(m_vision.getPixyXValue() == 158)
             return true;
-        else 
-            return false;
+        return false;
     }
 }
