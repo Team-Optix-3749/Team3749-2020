15b8be33b3c86ff3a09bdd794b08bc0eb835e3bf
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-03-05 18:45:47 -0800

    set idle mode for shooter

--- Team3749-2020/src/main/java/frc/robot/subsystems/NewShooter.java
+++ Team3749-2020/src/main/java/frc/robot/subsystems/NewShooter.java
@@ -3,9 +3,9 @@ package frc.robot.subsystems;
 import com.revrobotics.CANEncoder;
 import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.ControlType;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.ControlType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Robot;
@@ -24,6 +24,7 @@ public class NewShooter extends SubsystemBase {
     public NewShooter() {
         m_shooterMotor = new CANSparkMax(Robot.getConstants().getCAN("shooter_motor"), MotorType.kBrushless);
 
+        m_shooterMotor.setIdleMode(IdleMode.kCoast);
         m_encoder = m_shooterMotor.getEncoder();
         m_controller = m_shooterMotor.getPIDController();
         m_controller.setFeedbackDevice(m_encoder);
